d915e74840e8a0acfaad0fa0211e9cb3c7cf404b
denjay <295077487@qq.com>
2018-03-01 13:45:46 +0800

    改走马灯图片

--- src/views/Carousel.vue
+++ src/views/Carousel.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="wrapper">
       <el-carousel :interval="4000" type="card" height="200px">
-      <el-carousel-item v-for="article in articles_recommend" :key="article.index">
-        <router-link :to="'/articles/' + article.article_id"></router-link>
+      <el-carousel-item v-for="(send_word, index) in send_words" :key="send_word">
+        <b><p :style="{backgroundImage: 'url(/static/' + index + '.jpg)'}">{{ send_word }}</p></b>
+        <!-- <router-link :to="'/articles/' + article.article_id"></router-link> -->
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -12,12 +13,17 @@
 export default {
   data() {
     return {
-      articles_recommend: [
-        { article_id: 1, pic: '1.jpg'},
-        { article_id: 2, pic: '2.jpg'},
-        { article_id: 3, pic: '3.jpg'},
-        { article_id: 4, pic: '4.jpg'},
+      send_words: [
+        "真理惟一可靠的标准就是永远自相符合",
+        "世界上一成不变的东西，只有“任何事物都是在不断变化的”这条真理",
+        "没有人不爱惜他的生命，但很少人珍视他的时间",
+        "毫无经验的初恋是迷人的，但经得起考验的爱情是无价的",
       ]
+    }
+  },
+  methods: {
+    to() {
+      this.$router.push("/articles/1")
     }
   }
 }
@@ -30,15 +36,17 @@ export default {
     padding: 10px;
     background-color: #83a3c79f
  }
+ p {
+   padding: 40px;
+   color: white;
+   height: 100%;
+   background-size: cover;
+ }
 </style>
 
 <style>
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
-    line-height: 200px;
-    margin: 0;
+  .el-carousel__item {
+    border-radius: 5px;
   }
   .el-carousel__item:nth-child(2n) {
     background-color: #99a9bf;
